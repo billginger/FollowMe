67b8b2dce961c45d4a13fa11019f2c251c44ead9
Bill <jfg@163.com>
2023-07-23 21:08:13 +0800

    No need respond to self

--- FollowMe.lua
+++ FollowMe.lua
@@ -2,11 +2,13 @@ local f = CreateFrame("Frame")
 
 f:RegisterEvent("CHAT_MSG_PARTY")
 f:SetScript("OnEvent", function()
-  if arg1 == "follow me" then
-    FollowByName(arg2, true)
-    SendChatMessage("followed", "PARTY")
-  elseif arg1 == "stop" then
-    FollowUnit("player")
-    SendChatMessage("stoped", "PARTY")
+  if UnitName("player") ~= arg2 then
+    if arg1 == "follow me" then
+      FollowByName(arg2, true)
+      SendChatMessage("following", "PARTY")
+    elseif arg1 == "stop" then
+      FollowUnit("player")
+      SendChatMessage("stoped", "PARTY")
+    end
   end
 end)
